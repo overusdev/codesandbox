3f3aca9ed4ffe1b256d6e3f2246ecc3c3a55c7be
overusdev <overus.dev@gmail.com>
2022-09-06 11:59:19 +0000

    Update 8 files

--- dist/src.a2b27638.js
+++ dist/src.a2b27638.js
@@ -146,15 +146,31 @@ function _default() {
   });
   console.log(age, firstName);
 }
+},{}],"src/modules/rest/index.js":[function(require,module,exports) {
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.default = _default;
+
+function _default() {
+  var arr1 = ['Tom', 'Bob', 'John'];
+  var arr2 = ['Mary'].concat(arr1);
+  console.log(arr2);
+}
 },{}],"src/index.js":[function(require,module,exports) {
 "use strict";
 
 var _index = _interopRequireDefault(require("./modules/destruct/index"));
 
+var _index2 = _interopRequireDefault(require("./modules/rest/index"));
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 (0, _index.default)();
-},{"./modules/destruct/index":"src/modules/destruct/index.js"}],"node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
+(0, _index2.default)();
+},{"./modules/destruct/index":"src/modules/destruct/index.js","./modules/rest/index":"src/modules/rest/index.js"}],"node_modules/parcel-bundler/src/builtins/hmr-runtime.js":[function(require,module,exports) {
 var global = arguments[3];
 var OVERLAY_ID = '__parcel__error__overlay__';
 var OldModule = module.bundle.Module;
@@ -182,7 +198,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "45501" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "33317" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 import destruct from "./modules/destruct/index";
+import rest from './modules/rest/index';
 
 destruct();
+rest();
